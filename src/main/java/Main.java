b6d5a8fe6b85862103a305b87a5178e329e42075
Gorgios <jurek.r98@gmail.com>
2019-05-30 14:54:46 +0200

    finished DOM

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,39 @@
 import org.dom4j.DocumentException;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) throws DocumentException, IOException {
-        String filePath="src/main/resources/database.xml";
-        Parser par= new Parser(filePath);
-        par.getTeams();
+        String filePath = "src/main/resources/database.xml";
+        Parser par = new Parser(filePath);
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        while (a != 7) {
+            show();
+            a = sc.nextInt();
+            switch (a) {
+                case 1: par.teamMake(sc);break;
+                case 2: par.deleteTeam(sc);break;
+                case 3: par.modifyTeamName(sc); break;
+                case 4: par.modifyTeamRank(sc); break;
+                case 5: par.getTeams();break;
+                case 6: par.sort("//chessLeague/teams/team"); break;
+                case 7: break;
+                default: continue;
+            }
+        }
+
+    }
+
+    public static void show() {
+        System.out.println("MENU");
+        System.out.println("1. Dodaj druzyne");
+        System.out.println("2. Usun druzyne");
+        System.out.println("3. Zaaktualizuj nazwe druzyny");
+        System.out.println("4. Zaaktualizuj ranking druzyny");
+        System.out.println("5. Wypisz druzyny");
+        System.out.println("6. Sortuj druzyny");
+        System.out.println("7. Wyjdz");
     }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,9 +6,7 @@ import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
 import java.io.*;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Parser {
     private SAXReader reader;
@@ -19,6 +17,39 @@ public class Parser {
         this.document=reader.read(filePath);
     }
 
+    public void teamMake(Scanner sc) throws IOException {
+        System.out.println("Podaj ID teamu (3 litery, bez cyfr)");
+        sc.nextLine();
+        String id = sc.nextLine();
+        System.out.println("Podaj nazwe");
+        String name = sc.nextLine();
+
+        System.out.println("Podaj ranking");
+        int rank = sc.nextInt();
+        System.out.println("Podaj imie pierwszego trenera");
+        sc.nextLine();
+        String firstCoachFirstName = sc.nextLine();
+        System.out.println("Podaj nazwisko pierwszego trenera");
+        String firstCoachLastName = sc.nextLine();
+        System.out.println("Podaj imie trenera od analizy");
+        String analCoachFirstName = sc.nextLine();
+        System.out.println("Podaj nazwisko trenera od analizy");
+        String analCoachLastName = sc.nextLine();
+        System.out.println("Podaj imie trenera mlodzikow");
+        String youngCoachFirstName = sc.nextLine();
+        System.out.println("Podaj nazwisko trenera mlodzikow");
+        String youngCoachLastName = sc.nextLine();
+        System.out.println("Podaj ulice");
+        String street = sc.nextLine();
+
+        System.out.println("Podaj miasto");
+        String city= sc.nextLine();
+
+        System.out.println("Podaj kraj");
+        String country = sc.nextLine();
+        createTeam(id, name, rank, firstCoachFirstName, firstCoachLastName, analCoachFirstName, analCoachLastName, youngCoachFirstName, youngCoachLastName, street, city, country);
+
+    }
 
     public void createTeam(String id, String name, int rank, String firstCoachFirstName, String firstCoachLastName, String analCoachFirstName,
                            String analCoachLastName, String youngCoachFirstName, String youngCoachLastName, String street, String city, String country) throws IOException {
@@ -64,21 +95,52 @@ public class Parser {
         int a=1;
         for( Node node: nodes){
             System.out.println(a+". TeamID: "+node.valueOf("@id")
-                    + "  Name: " + node.selectSingleNode("name").getText()
-                    + "  Rank: " + node.selectSingleNode("rank").getText()
-                    +"   FirstCoach : "+node.selectSingleNode("staff/first_coach/firstname").getText()+node.selectSingleNode("staff/first_coach/lastname").getText()
-                    +"   Street: "+node.selectSingleNode("address/street").getText()
-                    +"   City: "+ node.selectSingleNode("address/city").getText()
-                    +"   Country: "+node.selectSingleNode("address/country").getText());
+                    + "  Naazwa: " + node.selectSingleNode("name").getText()
+                    + "  Ranking: " + node.selectSingleNode("rank").getText()
+                    +"   Trener : "+node.selectSingleNode("staff/first_coach/firstname").getText()+" " + node.selectSingleNode("staff/first_coach/lastname").getText()
+                    +"   Ulica: "+node.selectSingleNode("address/street").getText()
+                    +"   Miasto: "+ node.selectSingleNode("address/city").getText()
+                    +"   Kraj: "+node.selectSingleNode("address/country").getText());
             a++;
         }
         return nodes;
     }
-    public void deleteTeam(String id) throws IOException {
-        Node node= document.selectSingleNode("//ChessLeague/teams/team[@id = '"+id+"']");
-        node.detach();
+    public void modifyTeamName(Scanner sc) throws IOException {
+        System.out.println("Wpisz ID zespolu: ");
+        sc.nextLine();
+        String id = sc.nextLine();
+        Node node = this.document.selectSingleNode("//chessLeague/teams/team[@id = '"+id+"']");
+        System.out.println("Wpisz nowa nazwe zespolu: ");
+        String newName = sc.nextLine();
+        Element team = (Element) node;
+        team.element("name").setText(newName);
         print();
-        //save
+    }
+    public void modifyTeamRank(Scanner sc) throws IOException {
+        System.out.println("Wpisz ID zespolu: ");
+        sc.nextLine();
+        String id = sc.nextLine();
+        Node node = this.document.selectSingleNode("//chessLeague/teams/team[@id = '"+id+"']");
+        System.out.println("Wpisz nowy ranking zespolu: ");
+        int newRank = sc.nextInt();
+        Element team = (Element) node;
+        team.element("rank").setText(Integer.toString(newRank));
+        print();
+    }
+    public void deleteTeam(Scanner sc)  throws IOException {
+        System.out.println("Jaki zespol usunac (Podaj ID): ");
+        sc.nextLine();
+        String id = sc.nextLine();
+        try {
+            Node node = document.selectSingleNode("//chessLeague/teams/team[@id = '" + id + "']");
+            node.detach();
+            print();
+            System.out.println("Usunieto pomyslnie");
+        }
+        catch (Exception exc){
+            exc.printStackTrace();
+            System.out.println("Nie udalo sie usunac");
+        }
     }
 
     public void print() throws IOException {
@@ -90,4 +152,29 @@ public class Parser {
         writer.close();
     }
 
+    public void sort(String xPath) throws IOException {
+        List<Node> nodes = this.document.selectNodes(xPath);
+        Collections.sort(nodes, (Node n1, Node n2)->n1.getStringValue().compareTo(n2.getStringValue()));
+        int a=1;
+        for (Node node : nodes) {
+            System.out.println(a+". ID : "+node.valueOf("@id")
+                    + "  Nazwa: " + node.selectSingleNode("name").getText()
+                    + "  Ranking: " + node.selectSingleNode("rank").getText()
+                    +"  Trener: "+node.selectSingleNode("staff/first_coach/firstname").getText()+" " +node.selectSingleNode("staff/first_coach/lastname").getText()
+                    +"  Ulica: "+node.selectSingleNode("address/street").getText()
+                    +"  Miasto: "+ node.selectSingleNode("address/city").getText()
+                    +"  Kraj: "+node.selectSingleNode("address/country").getText());
+            a++;
+        }
+        print();
+    }
+    private String getContent(Element element) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Iterator<Element> i = element.elementIterator(); i.hasNext();) {
+            Element e = i.next();
+            stringBuilder.append(e.asXML());
+        }
+        stringBuilder.append("\n");
+        return stringBuilder.toString();
+    }
 }
